5bdb16c2cf05e378cbc4f301430c805751623736
yangkexin <yangkexin0528@163.com>
2023-07-18 13:17:24 +0800

    Create __init__.py

---
+++ models/__init__.py
@@ -0,0 +1,8 @@
+from .modeling_utils import (WEIGHTS_NAME, CONFIG_NAME, TF_WEIGHTS_NAME,
+                          PretrainedConfig, PreTrainedModel, prune_layer, Conv1D)
+
+from .file_utils import (PYTORCH_TRANSFORMERS_CACHE, PYTORCH_PRETRAINED_BERT_CACHE, cached_path)
+
+from .tokenization_gpt2 import GPT2Tokenizer
+
+from .tokenization_utils import (PreTrainedTokenizer)
