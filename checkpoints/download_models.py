61202addc83a04b33ae2fae2fd1c020f7eb9a89f
yangkexin <yangkexin0528@163.com>
2023-07-26 20:55:46 +0800

    Create download_models.py

---
+++ checkpoints/download_models.py
@@ -0,0 +1,19 @@
+from transformers import TrainingArguments, GPT2LMHeadModel, GPT2TokenizerFast, GPT2Config, Trainer, GPT2Model, AutoTokenizer,AutoModelForSequenceClassification
+#for prompts
+models = ['gpt2']
+for name in models:
+    model = GPT2Model.from_pretrained(name)  
+    tokenizer = AutoTokenizer.from_pretrained(name)
+    model.save_pretrained("{}".format(name))
+    tokenizer.save_pretrained("{}".format(name))
+    print("save model to "+"{}".format(name))
+
+#for sentiment classifer
+roberta=AutoModelForSequenceClassification.from_pretrained("roberta-large", num_labels=2)
+print(roberta)
+roberta.save_pretrained("roberta-large-2")
+
+#for food classifer
+roberta=AutoModelForSequenceClassification.from_pretrained("roberta-large", num_labels=3)
+print(roberta)
+roberta.save_pretrained("roberta-large-3")
