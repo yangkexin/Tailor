b0d40f7ccac069a775719326c01e4019b17ad2d5
yangkexin <yangkexin0528@163.com>
2024-01-09 17:32:39 +0800

    Update utils.py
    add creat prompts function

--- utils.py
+++ utils.py
@@ -16,3 +16,28 @@ def get_init_prefix_weight(init_prefix, base_model_name, input_embeddings, num_t
         embs = torch.cat((embs, embs), dim=0)
     embs = embs[-num_token_of_prefix:, :]
     return embs
+    
+def create_prompts(tokenizer_name, control_code=None, generate_eval=False):
+    # different prompts for eval and test. we use eval prompts to select models
+    prompts = ['Once upon a time', 'The book', 'The chicken', 'The city', 'The country', 'The house', 'The lake',
+               'The last time', 'The movie', 'The painting', 'The pizza', 'The potato', 'The president of the country',
+               'The road', 'The year is 1910.']#15
+    if generate_eval:
+        prompts = ['Anyone who wishes', 'As of', 'It is', 'In October 2014', 'While people can', 'Not only',
+                   'The park', 'The game', 'The reward', 'The paper']#10
+    tokenizer = None
+    if tokenizer_name == 'gpt2':
+        tokenizer = GPT2TokenizerFast.from_pretrained('gpt2')
+    elif tokenizer_name == 'bart':
+        tokenizer = BartTokenizerFast.from_pretrained('facebook/bart-base')
+    if tokenizer_name == 'ctrl':
+        assert control_code is not None, 'control_code can not be None'
+        final_prompts = []
+        for prompt in prompts:
+            final_prompts.append(control_code + ' ' + prompt)
+        prompts = final_prompts
+    prompts_tensors = []
+    for prompt in prompts:
+        prompts_tensors.append(tokenizer(prompt, return_tensors='pt').input_ids)
+
+    return prompts_tensors, prompts
