e7ca57134fb3f50ca69fe6062ee3383be012bbbc
yangkexin <yangkexin0528@163.com>
2024-01-15 10:15:10 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -85,3 +85,15 @@ def get_sorted_index(txt):
         return 3
     return 0
 
+def get_init_prefix_weight(init_prefix, base_model_name, input_embeddings, num_token_of_prefix):
+    tokenizer = AutoTokenizer.from_pretrained(base_model_name)
+    if init_prefix.endswith('unk'):
+        sent = tokenizer.unk_token
+    else:
+        sent = project_config.init_words[init_prefix]
+    input_ids = tokenizer(sent, return_tensors='pt').input_ids
+    embs = input_embeddings(input_ids)[0]
+    while embs.size(0) < num_token_of_prefix:
+        embs = torch.cat((embs, embs), dim=0)
+    embs = embs[-num_token_of_prefix:, :]
+    return embs
